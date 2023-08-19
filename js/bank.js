7c238045b24838699f5d2b0eb2de446118c81104
rabbani <rabbanisarkar642@gmail.com>
2023-08-19 16:14:26 +0600

    added the secend section

--- js/bank.js
+++ js/bank.js
@@ -3,10 +3,7 @@ const myemail=document.getElementById('email')
 const emaill = myemail.value ;   
 const myinput=document.getElementById('input')
 const password=myinput.value;
-if(emaill=='rabbanisarkar642@gmail.com'&& password=='rabbi'){
-    console.log('this is valid')
+
     window.location.href = 'web.html';
-}else{
-    console.log('not valid')
-}
+
 })
